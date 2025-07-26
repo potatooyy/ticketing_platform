e6c1e349337c474fa9593304b0e1855778b1bede
elmo <potato95101@gmail.com>
2025-07-26 15:42:59 +0000

    feat(settinga.py): 1. INSTALLED_APPS 新增 rest_framework 和 apps.users 2. AUTH_USER_MODEL = 'users.User'

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    # 'corsheaders',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
+     # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +124,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     # 其他允許跨域的前端網址
+# ]
