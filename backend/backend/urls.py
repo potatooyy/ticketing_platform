b7133802f367db5b336d45cdb1af9beb09ded5d8
elmo <potato95101@gmail.com>
2025-07-27 16:13:26 +0000

    feat(urls.py): 建立 /api /token 及 /api /token/refresh 路由

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,13 @@
 # apps/users/urls.py
 from django.contrib import admin
 from django.urls import path, include
+
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from apps.users.views import UserViewSet
 
 router = DefaultRouter()
@@ -10,4 +16,6 @@ router.register(r'users', UserViewSet, basename = 'user')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
