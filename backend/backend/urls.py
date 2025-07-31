753d338cd68c190a41228569aee5f2eb57f71831
elmo <potato95101@gmail.com>
2025-07-31 07:17:45 +0000

    feat(urls.py): add concerts, orders, tokens API routers; add MEDIA_URL

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,8 @@
 # apps/users/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
@@ -8,10 +10,21 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from apps.users.views import UserViewSet
+from apps.users.views import *
+from apps.concerts.views import *
+from apps.orders.views import *
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename = 'user')
+router.register(r'venues', VenueViewSet)
+router.register(r'seats', VenueSeatMapViewSet)
+router.register(r'concerts', ConcertViewSet)
+router.register(r'shows', ShowViewSet)
+router.register(r'pricings', PricingViewSet)
+router.register(r'tickets', TicketViewSet)
+router.register(r'orders', OrderViewSet)
+router.register(r'user/orders', UserOrderViewSet, basename='user-orders')
+router.register(r'admin/orders', AdminOrderViewSet, basename='admin-orders')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,3 +32,6 @@ urlpatterns = [
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
